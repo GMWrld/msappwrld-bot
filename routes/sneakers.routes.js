75c2588324b9599f71843388a8a141cf019f55c3
GMWrld <gm.wrld.13@gmail.com>
2025-08-07 12:40:59 +0300

    Missing controllers error

--- routes/sneakers.routes.js
+++ routes/sneakers.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getAllSneakers } = require('../controllers/sneakers.controller');
+const { getAllSneakers } = require('../controllers/sneakers.controllers');
 
 // GET /sneakers - Get all available sneakers
 router.get('/', getAllSneakers);
